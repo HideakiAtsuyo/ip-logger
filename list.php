3d02809ae8b2602782dd11b4ffc1563ac0773e8d
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-06-11 02:12:06 +0200

    Resolved :(

--- list.php
+++ list.php
@@ -9,21 +9,48 @@ require_once 'skid/db.php';
   <title>List of kids ;)</title>
 </head>
 <body style="background-color:#000000;">
-
-<!-- 00ff1c = H4X0R COLOR :P -->
-<?php
-$SQLGetUsers = $odb->query("SELECT * FROM `ips`");
-while ($getInfo = $SQLGetUsers->fetch(PDO::FETCH_ASSOC)) {
-    $ip       = $getInfo['ip'];
-    $ua     = $getInfo['UA'];
-    $time    = $getInfo['Time'];
-    echo '<p style="color: #00ff1c; text-align: center"><tr>
-    <td></td>
-    <td>' . $ip . '</td>
-    <td>' . $ua . '</td>
-    <td>' . $time . '</td>
-    </tr></p>';
-}
-?> 
+<style>
+  table
+  {
+    border-collapse: collapse;
+    background-color: #2f3136;
+    }
+    td, th
+    {
+      border: 2px solid black;
+      color: #0eff00;
+    }
+    caption
+    {
+      color: #0eff00;
+    }
+</style>
+<table class="table table-striped">
+  <caption color="#0eff00">VICTIMS</caption>
+  <thead>
+    <tr>
+      <th class="text-center" style="font-size: 20px;"></th>
+      <th style="font-size: 20px;">IP</th>
+      <th style="font-size: 20px;">UA(User-Agent)</th>
+      <th style="font-size: 20px;">TIME</th>
+    </tr>
+  </thead>
+  <tbody style="font-size: 24px;">
+    <?php
+    $GetKidsVictims = $odb->query("SELECT * FROM `ips`");
+    while ($oof = $GetKidsVictims->fetch(PDO::FETCH_ASSOC)) {
+      $ip       = $oof['ip'];
+      $ua     = $oof['UA'];
+      $time    = $oof['Time'];
+      echo '<tr>
+      <td></td>
+      <td>'.$ip.'</td>
+      <td>'.$ua.'</td>
+      <td>'.$time.'</td>
+      </tr>';
+    }
+    ?>  
+  </tbody>
+</table>
 </body>
 </html>

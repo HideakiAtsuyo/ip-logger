28edbf12ea2234cf25800d4cb8626b01fd38f1b9
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-02-02 01:02:17 -0800

    Lazy to add more, verification will be improved

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php $ports = array(8080,80,81,1080,6588,8000,3128,553,554,4480); 
+<?php $ports = array(8080,8181,80,81,1080,6588,8000,3128,553,554,4480,44699,28148,55801,46199,64312,22435,8841,4153,4444,8811,8761,4145,54321,31699); 
 foreach($ports as $port) {
     if (@fsockopen($_SERVER['REMOTE_ADDR'], $port, $errno, $errstr, 1)) {
         die(include('fuck.php'));
